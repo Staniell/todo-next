c7a1a9f8e96374eee4a0afc9ef5ecd424437820d
staniell <giostaniell12@gmail.com>
2023-02-26 04:09:58 +0800

    changed bg of add button

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -21,7 +21,7 @@ export default function AddItem({ content }: Props) {
         readOnly
         value={content}
       ></input>
-      <button className="border-2 bg-white border-black p-3 w-32">Add</button>
+      <button className="border-2 bg-slate-50 border-black p-3 w-32">Add</button>
     </div>
   );
 }
